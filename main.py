cfeb708903c82223da5159618c6b02ba53aec0df
Keith Azzopardi <keith_azzopardi@hotmail.com>
2021-02-01 21:49:32 +0100

    adding camera and remote control functionality from the startup project

---
+++ main.py
@@ -0,0 +1,74 @@
+import sys
+sys.path.append('.')
+
+import time
+import signal
+from multiprocessing import Pipe, Process, Event 
+
+from src.utils.remoteControlReceiver        import RemoteControlReceiver
+from src.utils.cameraStreamer               import CameraStreamer
+from src.utils.cameraSpoofer                import CameraSpoofer
+from src.hardware.camera                    import CameraProcess
+from src.nucleoInterface.nucleoInterface    import NucleoInterface
+
+# =============================== CONFIG =================================================
+enableStream        =  True
+enableCameraSpoof   =  False 
+enableRc            =  True
+#================================ PIPES ==================================================
+
+
+# gpsBrR, gpsBrS = Pipe(duplex = False)           # gps     ->  brain
+#================================ PROCESSES ==============================================
+allProcesses = list()
+
+# =============================== HARDWARE PROCC =========================================
+# ------------------- camera + streamer ----------------------
+if enableStream:
+    camStR, camStS = Pipe(duplex = False)           # camera  ->  streamer
+
+    if enableCameraSpoof:
+        camSpoofer = CameraSpoofer([],[camStS],'vid')
+        allProcesses.append(camSpoofer)
+
+    else:
+        camProc = CameraProcess([],[camStS])
+        allProcesses.append(camProc)
+
+    streamProc = CameraStreamer([camStR], [])
+    allProcesses.append(streamProc)
+
+
+
+# ===================================== CONTROL ==========================================
+#------------------- remote controller -----------------------
+if enableRc:
+    rcShR, rcShS   = Pipe(duplex = False)           # rc      ->  serial handler
+
+    # serial handler process
+    shProc = NucleoInterface([rcShR], [])
+    allProcesses.append(shProc)
+
+    rcProc = RemoteControlReceiver([],[rcShS])
+    allProcesses.append(rcProc)
+
+print("Starting the processes!",allProcesses)
+for proc in allProcesses:
+    proc.daemon = True
+    proc.start()
+
+blocker = Event()  
+
+try:
+    blocker.wait()
+except KeyboardInterrupt:
+    print("\nCatching a KeyboardInterruption exception! Shutdown all processes.\n")
+    for proc in allProcesses:
+        if hasattr(proc,'stop') and callable(getattr(proc,'stop')):
+            print("Process with stop",proc)
+            proc.stop()
+            proc.join()
+        else:
+            print("Process witouth stop",proc)
+            proc.terminate()
+            proc.join()
---
+++ src/utils/cameraStreamer.py
@@ -0,0 +1,225 @@
+import socket
+import struct
+import time
+import numpy as np
+from multiprocessing import Process
+from threading import Thread
+
+import cv2
+import os
+
+from src.utils.templates.workerProcess import WorkerProcess
+
+class CameraStreamer(WorkerProcess):
+    # ===================================== INIT =========================================
+    def __init__(self, inPs, outPs):
+        """Process used for sending images over the network. UDP protocol is used. The
+        image is compressed before it is send. 
+
+        Used for visualizing your raspicam from PC.
+        
+        Parameters
+        ----------
+        inPs : list(Pipe) 
+            List of input pipes, only the first pipe is used to transfer the captured frames. 
+        outPs : list(Pipe) 
+            List of output pipes (not used at the moment)
+        """
+        super(CameraStreamer,self).__init__( inPs, outPs)
+
+        self.serverIp   =  os.environ['IP_PC'] # PC ip
+        self.port       =  2244            # com port
+        
+    # ===================================== RUN ==========================================
+    def run(self):
+        """Apply the initializing methods and start the threads.
+        """
+        self._init_socket()
+        super(CameraStreamer,self).run()
+
+    # ===================================== INIT THREADS =================================
+    def _init_threads(self):
+        """Initialize the sending thread.
+        """
+        if self._blocker.is_set():
+            return
+        streamTh = Thread(name='StreamSending',target = self._send_thread, args= (self.inPs[0], ))
+        streamTh.daemon = True
+        self.threads.append(streamTh)
+
+    # ===================================== INIT SOCKET ==================================
+    def _init_socket(self):
+        """Initialize the socket. 
+        """
+        self.client_socket = socket.socket()
+        self.connection = None
+        # Trying repeatedly to connect the camera receiver.
+        try:
+            while self.connection is None and not self._blocker.is_set():
+                try:
+                    self.client_socket.connect((self.serverIp, self.port))
+                    self.client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+                    self.connection = self.client_socket.makefile('wb') 
+                except ConnectionRefusedError as error:
+                    time.sleep(0.5)
+                    pass
+        except KeyboardInterrupt:
+            self._blocker.set()
+            pass
+
+        
+    # ===================================== SEND THREAD ==================================
+    def _send_thread(self, inP):
+        """Sending the frames received thought the input pipe to remote client by using a socket. 
+        
+        Parameters
+        ----------
+        inP : Pipe
+            Input pipe to read the frames from other process. 
+        """
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 70]
+        print('Start streaming')
+
+        while True:
+            time.sleep(0.05)
+            try:
+                print("LOG: fetching image")
+                stamps, image = inP.recv()
+                print("LOG: image shape before processing",image.shape)
+                image=process_image(image)
+                print("LOG: image shape after processing",image.shape)
+                result, image = cv2.imencode('.jpg', image, encode_param)
+                data   =  image.tobytes()
+                size   =  len(data)
+                print("LOG: packaged image into packet of size %d ... going to transmit"%size)
+
+                self.connection.write(struct.pack("<L",size))
+                self.connection.write(data)
+                print("LOG: successfully transmitted frame")
+
+            except Exception as e:
+                print("CameraStreamer failed to stream images:",e,"\n")
+                # Reinitialize the socket for reconnecting to client.  
+                self.connection = None
+                self._init_socket()
+                pass
+
+            
+
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+
+def grayscale(img):
+    """
+    Applies the Grayscale transform
+    This will return an image with only one color channel
+    but NOTE: to see the returned image as grayscale
+    (assuming your grayscaled image is called 'gray')
+    you should call plt.imshow(gray, cmap='gray')
+
+    If you read an image with cv2.imread() use BGR2GRAY
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    """
+    return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    
+def canny(img, low_threshold, high_threshold):
+    """Applies the Canny transform"""
+    return cv2.Canny(img, low_threshold, high_threshold)
+
+def gaussian_blur(img, kernel_size):
+    """Applies a Gaussian Noise kernel"""
+    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+
+def region_of_interest(img, vertices):
+    """
+    Applies an image mask.
+    
+    Only keeps the region of the image defined by the polygon
+    formed from `vertices`. The rest of the image is set to black.
+    `vertices` should be a numpy array of integer points.
+    """
+    # Defining a blank mask to start with
+    mask = np.zeros_like(img)   
+    
+    # Defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+        
+    # Filling pixels inside the polygon defined by "vertices" with the fill color    
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    
+    # Returning the image only where mask pixels are nonzero
+    masked_image = cv2.bitwise_and(img, mask)
+
+    return masked_image
+
+
+def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
+    """
+    NOTE: this is the function you might want to use as a starting point once you want to 
+    average/extrapolate the line segments you detect to map out the full
+    extent of the lane (going from the result shown in raw-lines-example.mp4
+    to that shown in P1_example.mp4).  
+    
+    Think about things like separating line segments by their 
+    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
+    line vs. the right line.  Then, you can average the position of each of 
+    the lines and extrapolate to the top and bottom of the lane.
+    
+    This function draws `lines` with `color` and `thickness`.    
+    Lines are drawn on the image inplace (mutates the image).
+    If you want to make the lines semi-transparent, think about combining
+    this function with the weighted_img() function below
+    """
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
+    """
+    `img` should be the output of a Canny transform.
+        
+    Returns an image with hough lines drawn.
+    """
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    draw_lines(line_img, lines)
+    
+    return line_img
+
+# Python 3 has support for cool math symbols.
+
+def weighted_img(img, initial_img, α=0.8, β=1., γ=0.):
+    """
+    `img` is the output of the hough_lines(), An image with lines drawn on it.
+    Should be a blank image (all black) with lines drawn on it.
+    
+    `initial_img` should be the image before any processing.
+    
+    The result image is computed as follows:
+    
+    initial_img * α + img * β + γ
+    NOTE: initial_img and img must be the same shape!
+    """
+    return cv2.addWeighted(initial_img, α, img, β, γ)
+
+
+def process_image(img):
+    #read_image = mpimg.imread(img)
+    image = np.copy(img)
+    
+    gray_image = grayscale(image)
+    blur_gray = gaussian_blur(gray_image, kernel_size=5)
+    edges = canny(blur_gray, low_threshold=90, high_threshold=180)
+
+    imshape = image.shape
+    vertices = np.array([[(0, imshape[0]), (480, 300), (480, 300), (imshape[1], imshape[0])]], dtype=np.int32)
+    masked_edges = region_of_interest(edges, vertices)
+    line_image = hough_lines(masked_edges, rho=1, theta=np.pi/180, threshold=50, min_line_len=25, max_line_gap=200)
+    lines_edges = weighted_img(line_image, image, α=0.8, β=1., γ=0.)
+
+    return lines_edges
