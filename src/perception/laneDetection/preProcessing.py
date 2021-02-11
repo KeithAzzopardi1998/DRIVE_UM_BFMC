2b2a17ff33c9617b63a3367e329c259b78d2690c
Chris Bonnici <chris.bonnici@hotmail.co.uk>
2021-02-11 19:58:53 +0100

    added more preprocessing functions

--- src/perception/laneDetection/preProcessing.py
+++ src/perception/laneDetection/preProcessing.py
@@ -1,6 +1,8 @@
 import numpy as np 
 import cv2 
 import math
+from scipy import stats
+from collections import deque
 
 def to_hsl(img):
 	return cv2.cvtColor(img,cv2.COLOR_RGB2HLS)
@@ -36,7 +38,7 @@ def filter_img_hsl(img):
     hsl_img = to_hsl(img)
     hsl_yellow = isolate_yellow_hsl(hsl_img)
     hsl_white = isolate_white_hsl(hsl_img)
-    return combine_hsl_isolated_with_original(img, hsl_yellow, hsl_white)
+    return combine_hsl(img, hsl_yellow, hsl_white)
 
 def grayscale(img):
     return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -91,7 +93,83 @@ def getROI(img):
 	#return image only where mask nonzero
 	return cv2.bitwise_and(img, mask)
 
+def hough_transform(canny_img, rho, theta, threshold, min_line_len, max_line_gap):
+    return cv2.HoughLinesP(canny_img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
 
+def separate_lines(lines, img):
+    img_shape = img.shape
+    
+    middle_x = img_shape[1] / 2
+    
+    left_lane_lines = []
+    right_lane_lines = []
 
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            dx = x2 - x1 
+            if dx == 0:
+                #Discarding line since we can't gradient is undefined at this dx
+                continue
+            dy = y2 - y1
+            
+            # Similarly, if the y value remains constant as x increases, discard line
+            if dy == 0:
+                continue
+            
+            slope = dy / dx
+            
+            # This is pure guess than anything... 
+            # but get rid of lines with a small slope as they are likely to be horizontal one
+            epsilon = 0.1
+            if abs(slope) <= epsilon:
+                continue
+            
+            if slope < 0 and x1 < middle_x and x2 < middle_x:
+                # Lane should also be within the left hand side of region of interest
+                left_lane_lines.append([[x1, y1, x2, y2]])
+            elif x1 >= middle_x and x2 >= middle_x:
+                # Lane should also be within the right hand side of region of interest
+                right_lane_lines.append([[x1, y1, x2, y2]])
+    
+    return left_lane_lines, right_lane_lines
 
+def getLanesFormula(lines):
+    xs = []
+    ys = []
+    
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            xs.append(x1)
+            xs.append(x2)
+            ys.append(y1)
+            ys.append(y2)
+    
+    slope, intercept, r_value, p_value, std_err = stats.linregress(xs, ys)
+    
+    # a straight line is expressed as f(x) = Ax + b. Slope is the A, while intercept is the b
+    return (slope, intercept)
+
+def create_coefficients_list(length = 10):
+    return deque(maxlen=length)
+
+def mean_coefficients(coefficients_queue, axis=0):        
+	return [0, 0] if len(coefficients_queue) == 0 else np.mean(coefficients_queue, axis=axis)
+
+def determine_line_coefficients(stored_coefficients, current_coefficients, MAXIMUM_SLOPE_DIFF=0.1, MAXIMUM_INTERCEPT_DIFF=50.0):
+	
+	if len(stored_coefficients)==0:
+		stored_coefficients.append(current_coefficients)
+		return current_coefficients
+	mean = mean_coefficients(stored_coefficients)
+	abs_slope_diff = abs(current_coefficients[0] - mean[0])
+	abs_intercept_diff = abs(current_coefficients[1] - mean[1])
+
+	if abs_slope_diff > MAXIMUM_SLOPE_DIFF or abs_intercept_diff > MAXIMUM_INTERCEPT_DIFF:
+		# Identified big difference in slope 
+        # In this case use the mean
+		return mean
+	else:
+		# Save our coefficients and returned a smoothened one
+		stored_coefficients.append(current_coefficients)
+		return mean_coefficients(stored_coefficients)
 
