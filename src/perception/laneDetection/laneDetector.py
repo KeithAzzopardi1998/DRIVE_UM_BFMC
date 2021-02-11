71b63427e285c70eda9bef0577d47f1e0cc2ee28
Chris Bonnici <chris.bonnici@hotmail.co.uk>
2021-02-11 20:10:59 +0100

    added lane detection pipeline function

--- src/perception/laneDetection/laneDetector.py
+++ src/perception/laneDetection/laneDetector.py
@@ -41,5 +41,43 @@ class LaneDetector(WorkerProcess):
 
 
 	#======================= METHODS =======================
-	def laneDetection(self,img):
-		
+	def laneDetection(self, img_in):
+		# Setting Hough Transform Parameters
+		rho = 1 # 1 degree
+		theta = (np.pi/180) * 1
+		threshold = 15
+		min_line_length = 20
+		max_line_gap = 10
+
+		left_lane_coefficients  = pp.create_coefficients_list()
+        right_lane_coefficients = pp.create_coefficients_list()
+        
+        previous_left_lane_coefficients = None
+        previous_right_lane_coefficients = None
+
+
+        # Begin lane detection pipiline
+        img = image_in.copy()
+        combined_hsl_img = pp.filter_img_hsl(img)
+        grayscale_img = pp.grayscale(combined_hsl_img)
+        gaussian_smoothed_img = pp.gaussian_blur(grayscale_img, kernel_size=5)
+        canny_img = cv2.Canny(gaussian_smoothed_img, 50, 150)
+        segmented_img = pp.getROI(canny_img)
+        hough_lines = hough_transform(segmented_img, rho, theta, threshold, min_line_length, max_line_gap)
+
+        try:
+			left_lane_lines, right_lane_lines = pp.separate_lines(hough_lines, img)
+			left_lane_slope, left_intercept = pp.getLanesFormula(left_lane_lines)
+			right_lane_slope, right_intercept = pp.getLanesFormula(right_lane_lines)
+			smoothed_left_lane_coefficients = pp.determine_line_coefficients(left_lane_coefficients, [left_lane_slope, left_intercept])
+			smoothed_right_lane_coefficients = pp.determine_line_coefficients(right_lane_coefficients, [right_lane_slope, right_intercept])
+
+			return smoothed_left_lane_coefficients, smoothed_right_lane_coefficients
+
+		except Exception as e:
+			#print("*** Error - will use saved coefficients ", e)
+			smoothed_left_lane_coefficients = pp.determine_line_coefficients(left_lane_coefficients, [0.0, 0.0])
+			smoothed_right_lane_coefficients = pp.determine_line_coefficients(right_lane_coefficients, [0.0, 0.0])
+
+			return smoothed_left_lane_coefficients, smoothed_right_lane_coefficients
+
