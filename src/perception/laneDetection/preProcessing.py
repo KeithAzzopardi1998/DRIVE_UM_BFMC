46eba72ab829fcd6d9101a1ff49c53174c519965
keith <keith.azzopardi.16@um.edu.mt>
2021-02-18 17:45:57 +0100

    minor syntax  & whitespace fixes

--- src/perception/laneDetection/preProcessing.py
+++ src/perception/laneDetection/preProcessing.py
@@ -5,31 +5,31 @@ from scipy import stats
 from collections import deque
 
 def to_hsl(img):
-	return cv2.cvtColor(img,cv2.COLOR_RGB2HLS)
+    return cv2.cvtColor(img,cv2.COLOR_RGB2HLS)
 
 def isolate_yellow_hsl(img):
-	# Lower value equivalent pure HSL is (30, 45, 15)
-	low_threshold = np.array([15, 38, 115], dtype=np.uint8)
-	# Higher value equivalent pure HSL is (75, 100, 80)
-	high_threshold = np.array([35, 204, 255], dtype=np.uint8)
+    # Lower value equivalent pure HSL is (30, 45, 15)
+    low_threshold = np.array([15, 38, 115], dtype=np.uint8)
+    # Higher value equivalent pure HSL is (75, 100, 80)
+    high_threshold = np.array([35, 204, 255], dtype=np.uint8)
 
-	yellow_mask = cv2.inRange(img, low_threshold, high_threshold)
+    yellow_mask = cv2.inRange(img, low_threshold, high_threshold)
 
-	return yellow_mask
+    return yellow_mask
 
-def isolate_while_hsl(img):
-	# Lower value equivalent pure HSL is (30, 45, 15)
-	low_threshold = np.array([0, 200, 0], dtype=np.uint8)
-	# Higher value equivalent pure HSL is (360, 100, 100)
-	high_threshold = np.array([180, 255, 255], dtype=np.uint8)
+def isolate_white_hsl(img):
+    # Lower value equivalent pure HSL is (30, 45, 15)
+    low_threshold = np.array([0, 200, 0], dtype=np.uint8)
+    # Higher value equivalent pure HSL is (360, 100, 100)
+    high_threshold = np.array([180, 255, 255], dtype=np.uint8)
 
-	white_mask = cv2.inRange(img, low_threshold, high_threshold)
+    white_mask = cv2.inRange(img, low_threshold, high_threshold)
 
-	return white_mask
+    return white_mask
 
 def combine_hsl(img, hsl_yellow, hsl_white):
-	hsl_mark = cv2.bitwise_or(hsl_yellow,hsl_white)
-	return cv2.bitwise_and(img, img, mask=hsl_mask)
+    hsl_mask = cv2.bitwise_or(hsl_yellow,hsl_white)
+    return cv2.bitwise_and(img, img, mask=hsl_mask)
 
 def filter_img_hsl(img):
     '''
@@ -50,48 +50,48 @@ def gaussian_blur(grayscale_img, kernel_size=5):
     return cv2.GaussianBlur(grayscale_img, (kernel_size, kernel_size), 0)
 
 def getVertices(img):
-	"""This functions needs to be hard coded for
-	our resolution - might need tuning
-	"""
-	img_shape = img.shape
-	height = img_shape[0]
-	width = img_shape[1]
+    """This functions needs to be hard coded for
+    our resolution - might need tuning
+    """
+    img_shape = img.shape
+    height = img_shape[0]
+    width = img_shape[1]
 
-	vert = None
+    vert = None
 
-	if (width, height) == (960, 540):
-	    region_bottom_left = (130 ,img_shape[0] - 1)
-	    region_top_left = (410, 330)
-	    region_top_right = (650, 350)
-	    region_bottom_right = (img_shape[1] - 30,img_shape[0] - 1)
-	    vert = np.array([[region_bottom_left , region_top_left, region_top_right, region_bottom_right]], dtype=np.int32)
-	else:
-		region_bottom_left = (200 , 680)
-	    region_top_left = (600, 450)
-	    region_top_right = (750, 450)
-	    region_bottom_right = (1100, 650)
-	    vert = np.array([[region_bottom_left , region_top_left, region_top_right, region_bottom_right]], dtype=np.int32)
+    if (width, height) == (960, 540):
+        region_bottom_left = (130 ,img_shape[0] - 1)
+        region_top_left = (410, 330)
+        region_top_right = (650, 350)
+        region_bottom_right = (img_shape[1] - 30,img_shape[0] - 1)
+        vert = np.array([[region_bottom_left , region_top_left, region_top_right, region_bottom_right]], dtype=np.int32)
+    else:
+        region_bottom_left = (200 , 680)
+        region_top_left = (600, 450)
+        region_top_right = (750, 450)
+        region_bottom_right = (1100, 650)
+        vert = np.array([[region_bottom_left , region_top_left, region_top_right, region_bottom_right]], dtype=np.int32)
 
-	return vert
+    return vert
 
 def getROI(img):
-	#define blank mask
-	mask=np.zeros_like(img)
+    #define blank mask
+    mask=np.zeros_like(img)
 
-	#defining 3 channel or 1 channel color to fill mask
-	if len(img.shape) > 2:
-		channel_count = img.shape[2] # i.e. 3 or 4
-		ignore_mask_color = (255,) * channel_count
-	else:
-		ignore_mask_color = 255
+    #defining 3 channel or 1 channel color to fill mask
+    if len(img.shape) > 2:
+        channel_count = img.shape[2] # i.e. 3 or 4
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
 
-	vert = getVertices(img)
+    vert = getVertices(img)
 
-	#filling pixels inside the polygon defined by "vertices" with the fill color
-	cv2.fillPoly(mask, vert, ignore_mask_color)
+    #filling pixels inside the polygon defined by "vertices" with the fill color
+    cv2.fillPoly(mask, vert, ignore_mask_color)
 
-	#return image only where mask nonzero
-	return cv2.bitwise_and(img, mask)
+    #return image only where mask nonzero
+    return cv2.bitwise_and(img, mask)
 
 def hough_transform(canny_img, rho, theta, threshold, min_line_len, max_line_gap):
     return cv2.HoughLinesP(canny_img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
@@ -153,23 +153,23 @@ def create_coefficients_list(length = 10):
     return deque(maxlen=length)
 
 def mean_coefficients(coefficients_queue, axis=0):
-	return [0, 0] if len(coefficients_queue) == 0 else np.mean(coefficients_queue, axis=axis)
+    return [0, 0] if len(coefficients_queue) == 0 else np.mean(coefficients_queue, axis=axis)
 
 def determine_line_coefficients(stored_coefficients, current_coefficients, MAXIMUM_SLOPE_DIFF=0.1, MAXIMUM_INTERCEPT_DIFF=50.0):
 
-	if len(stored_coefficients)==0:
-		stored_coefficients.append(current_coefficients)
-		return current_coefficients
-	mean = mean_coefficients(stored_coefficients)
-	abs_slope_diff = abs(current_coefficients[0] - mean[0])
-	abs_intercept_diff = abs(current_coefficients[1] - mean[1])
+    if len(stored_coefficients)==0:
+        stored_coefficients.append(current_coefficients)
+        return current_coefficients
+    mean = mean_coefficients(stored_coefficients)
+    abs_slope_diff = abs(current_coefficients[0] - mean[0])
+    abs_intercept_diff = abs(current_coefficients[1] - mean[1])
 
-	if abs_slope_diff > MAXIMUM_SLOPE_DIFF or abs_intercept_diff > MAXIMUM_INTERCEPT_DIFF:
-		# Identified big difference in slope
+    if abs_slope_diff > MAXIMUM_SLOPE_DIFF or abs_intercept_diff > MAXIMUM_INTERCEPT_DIFF:
+        # Identified big difference in slope
         # In this case use the mean
-		return mean
-	else:
-		# Save our coefficients and returned a smoothened one
-		stored_coefficients.append(current_coefficients)
-		return mean_coefficients(stored_coefficients)
+        return mean
+    else:
+        # Save our coefficients and returned a smoothened one
+        stored_coefficients.append(current_coefficients)
+        return mean_coefficients(stored_coefficients)
 
